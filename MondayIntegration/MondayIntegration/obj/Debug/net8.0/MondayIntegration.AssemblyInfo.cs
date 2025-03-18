9b5fff424fc2135a2c99f3fc7fd17372c2728f75
kamomohapi <kamomohapi17@gmail.com>
2025-03-18 20:57:50 +0200

    added docker for render deployment

--- MondayIntegration/MondayIntegration/obj/Debug/net8.0/MondayIntegration.AssemblyInfo.cs
+++ MondayIntegration/MondayIntegration/obj/Debug/net8.0/MondayIntegration.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0cfc12d6-0133-46f0-80b4-b912f09a6b7e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MondayIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f7f3e71b52f9de600bc25fc34eae42e6f60c7ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MondayIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MondayIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
